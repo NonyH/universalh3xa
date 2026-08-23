751fe8aafd75ec5ad24e6c54a5ed82de47eff2a3
NonyH <nonycyv@gmail.com>
2026-08-23 14:02:11 -0400

    Add loaders.lua with configuration for asset

---
+++ loaders.lua
@@ -0,0 +1,23 @@
+return {
+
+    {
+        Title = "H3X4 X",
+
+        Image = "rbxassetid://97057058128005",
+
+        URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/h3xaX",
+
+        Enabled = true,
+    },
+
+    {
+        Title = "HX Boat",
+
+        Image = "rbxassetid://114285884839092",
+
+        URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/hboat.lua",
+
+        Enabled = true,
+    },
+
+}
