e66f9a6af420a0cd6feea505adf9028d68a79c35
NonyH <nonycyv@gmail.com>
2026-08-23 15:57:16 -0400

    Update loaders.lua

--- loaders.lua
+++ loaders.lua
@@ -1,23 +1,23 @@
 return {
-
     {
         Title = "H3X4 X",
 
+        DescriptionES = "Hexa X es un script semiuniversal para Roblox que reúne funciones de combate, movimiento, visualización y personalización en una interfaz moderna, sencilla y compatible con PC y dispositivos móviles. Incluye herramientas como Aimbot, ESP, Trigger Bot, X-Ray, Fullbright, ajustes de movimiento y otras opciones configurables.",
+        DescriptionEN = "Hexa X is a semi-universal script for Roblox that combines combat, movement, visualization, and customization features into a modern, simple interface compatible with both PC and mobile devices. It includes tools such as Aimbot, ESP, Trigger Bot, X-Ray, Fullbright, movement adjustments, and other configurable options.",
+
         Image = "rbxassetid://97057058128005",
-
         URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/h3xaX",
-
         Enabled = true,
     },
 
     {
         Title = "HX Boat",
 
+        DescriptionES = "Hola.",
+        DescriptionEN = "hola2.",
+
         Image = "rbxassetid://114285884839092",
-
         URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/hboat.lua",
-
         Enabled = true,
     },
-
 }
