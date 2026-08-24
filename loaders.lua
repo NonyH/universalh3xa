eda1ea6ae2db43e33d2f1e6f9c6fe349ed3fc9bc
NonyH <nonycyv@gmail.com>
2026-08-23 20:42:53 -0400

    Add new script entries for H3X4 X and HX Boat

--- loaders.lua
+++ loaders.lua
@@ -3,10 +3,21 @@ return {
         Title = "H3X4 X",
 
         DescriptionES = "Hexa X es un script semiuniversal para Roblox que reúne funciones de combate, movimiento, visualización y personalización en una interfaz moderna, sencilla y compatible con PC y dispositivos móviles. Incluye herramientas como Aimbot, ESP, Trigger Bot, X-Ray, Fullbright, ajustes de movimiento y otras opciones configurables.",
+
         DescriptionEN = "Hexa X is a semi-universal script for Roblox that combines combat, movement, visualization, and customization features into a modern, simple interface compatible with both PC and mobile devices. It includes tools such as Aimbot, ESP, Trigger Bot, X-Ray, Fullbright, movement adjustments, and other configurable options.",
 
         Image = "rbxassetid://97057058128005",
         URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/h3xaX",
+
+        Author = "Nony",
+        Game = "Universal / Semi-Universal",
+        Version = "1.0",
+
+        Tag = {
+            Title = "none",
+            Color = "#FFFFFF",
+        },
+
         Enabled = true,
     },
 
@@ -14,10 +25,21 @@ return {
         Title = "HX Boat",
 
         DescriptionES = "Script optimizado para Build A Boat con Auto Farm, teleports, mejoras de barco y utilidades.",
+
         DescriptionEN = "Optimized script for Build A Boat featuring Auto Farm, teleports, boat upgrades, and utilities.",
 
         Image = "rbxassetid://114285884839092",
         URL = "https://raw.githubusercontent.com/NonyH/universalh3xa/refs/heads/main/hboat.lua",
+
+        Author = "Nony",
+        Game = "Build A Boat For Treasure",
+        Version = "1.0",
+
+        Tag = {
+            Title = "NUEVO",
+            Color = "#FFFFFF",
+        },
+
         Enabled = true,
     },
 }
